250924704b90493fa98e190f78bda2fe7a7d6d70
Jang Se Hui <jsxhxx0819@gmail.com>
2023-07-20 14:28:27 +0900

    230719 수업 도시락통 완

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 window.onload = function () {
-  // AOS 셋팅
+  // 모달창 기능
+  let modalWrap = document.querySelector(".modal-wrap");
+  let modalClose = document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modalWrap.classList.add("fadeOut");
+    modalWrap.addEventListener("animationend", function () {
+      modalWrap.style.display = "none";
+    });
+  });
+  // AOS셋팅
   AOS.init();
   // 상단 스크롤 기능
   const header = document.querySelector(".header");
   const mbt = document.querySelector(".mbt");
   let scy = 0;
-  // 1. 스크롤바의 픽셀 위치값을 파악해서
+  // 1.스크롤바의 픽셀 위치값을 파악해서
   scy = window.document.documentElement.scrollTop;
-  // 2. class 적용
+  // 2.class적용
   if (scy > 0) {
     header.classList.add("active");
     mbt.classList.add("active");
   }
   window.addEventListener("scroll", function () {
-    scy = window.document.documentElement.scrollTop;
+    scy = this.document.documentElement.scrollTop;
     if (scy > 0) {
       header.classList.add("active");
       mbt.classList.add("active");
     } else {
-      // 스크롤이 되지 않은 상태이면서 !!!!!!
       const state = navMb.classList.contains("active");
       if (state) {
         // 만약에 모바일 메뉴가 펼쳐진 상태라면
@@ -35,7 +43,7 @@ window.onload = function () {
   const htmlRoot = document.querySelector("html");
   const navMb = document.querySelector(".nav-mb");
   mbt.addEventListener("click", function () {
-    // 현재 ani 클래스가 있는지 없는지 파악
+    // 현재 ani클래스가 있는지 없는지 파악
     const state = this.classList.contains("ani");
     if (state) {
       this.classList.remove("ani");
@@ -61,36 +69,42 @@ window.onload = function () {
   // 반응형 처리
   let winW = window.innerWidth;
   window.addEventListener("resize", function () {
+    // 웹브라우저 안쪽 너비
     winW = window.innerWidth;
+    // mobile ===> pc 전환
     if (winW > 1024) {
       mbt.classList.remove("ani");
-      htmlRoot.classList.remove("avtive");
+      htmlRoot.classList.remove("active");
       navMb.classList.remove("active");
+
       if (scy > 0) {
+        // 스크롤이 된 상태에서 화면 리사이징..
         header.classList.add("active");
         mbt.classList.add("active");
       } else {
+        // 스크롤 안됨. 화면 리사이징..
         header.classList.remove("active");
         mbt.classList.remove("active");
       }
     }
   });
-  //비주얼 슬라이드
-  //1.슬라이드(.swiper-slide)개수 만큼 li 생성하기
+  // 비주얼 슬라이드
+  // 1.슬라이드 (.swiper-slide)개수 만큼 li생성하기
   const swSlideCount = document.querySelectorAll(
     ".sw-visual .swiper-slide"
   ).length;
-  //2. li태그 출력 장소(ul 태그)저장
+  // 2.li태그 출력 장소(ul태그 )저장
   const swSlidePgUl = document.querySelector(".sw-visual-pg-list");
-  //3. li에 html로 작성할 글자를 생성한다.
+  //3.li에 html로 작성할 글자를 생성한다.
   let swVisualHtml = ``;
   for (let i = 0; i < swSlideCount; i++) {
     swVisualHtml = swVisualHtml + `<li>${i + 1}</li>`;
   }
-  //4.html을 추가한다.
+  // 4.html을 추가한다
   swSlidePgUl.innerHTML = swVisualHtml;
-  //5.페이지네이션 관련(코딩으로 생성한 li태그 저장)
+  // 5.페이지네이션 관련(코딩으로 생성한 li태그 저장)
   const swVisualPgLi = document.querySelectorAll(".sw-visual-pg-list > li");
+  // console.log(swVisualPgLi);
 
   var swiper = new Swiper(".sw-visual", {
     effect: "fade",
@@ -112,24 +126,24 @@ window.onload = function () {
       prevEl: ".sw-visual-prev",
     },
   });
-  // swiper가 최초 실행될 때
-  // 1번 li의 흰색 라인이 늘어나는 모션을 실행
+  // swiper 가 최초 실행될때
+  // 1번 li 의 흰색 라인이 늘어나는 모션을 실행
   swVisualPgLi[0].classList.add("active");
   // swiper가 바뀔때 마다 실행
   // 슬라이더가 바뀌는 상태를 찾아서
   // 우리가 적용하고자 하는 처리를 하고
-  // swiper의 API를 창조해서 작성
+  // swiper의 API를 참조해서 작성
   swiper.on("slideChange", function () {
     swVisualPgLi.forEach(function (item, index) {
       if (swiper.realIndex === index) {
-        // realIndex는 진짜 html 태그의 순서값
+        // realIndex는 진짜 html태그의 순서값
         item.classList.add("active");
       } else {
         item.classList.remove("active");
       }
     });
   });
-  // li태그를 클릭하면 처리하기
+  // li태그를 클릭 하면 처리하기
   swVisualPgLi.forEach(function (item, index) {
     item.addEventListener("click", function () {
       swiper.slideToLoop(index, 500, false);
@@ -158,7 +172,7 @@ window.onload = function () {
   gotop.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
-  // footer의 상단위치 픽셀값 파악.
+  // footer의 상단 위치 픽셀값 파악
   let footer = document.querySelector(".footer");
   let footerY = footer.offsetTop;
   let waypoint_footer = new Waypoint({
